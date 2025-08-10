bc61d883c795f9767b831b079a9dd593de54ff9d
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-08-10 23:12:48 +0500

    ref: nothing

--- core/middlewares.py
+++ core/middlewares.py
@@ -22,7 +22,8 @@ SKIP_PATHS = (
     '/api/v1/advertisement/favorite/',
     '/api/v1/cart-item/',
     add_api('role-manager'),
-    '/api/v1/product/upload-image/'
+    '/api/v1/product/upload-image/',
+
 )
 
 

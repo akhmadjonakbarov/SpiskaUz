40980b1d234bb092293f685cecf14962ea2bbd0a
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-08-17 07:58:53 +0500

    feat: supplier was updated

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = env.bool("DEBUG", default=True)
+DEBUG = env.bool("DEBUG", default=False)
 TESTING = env.bool("TESTING", default=False)
 SECRET_KEY = env.str("SECRET_KEY")
 OTP_EMAIL = env.str("OTP_EMAIL")

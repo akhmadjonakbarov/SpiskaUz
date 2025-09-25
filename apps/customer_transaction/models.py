52c602aa4826e0b4726330e3dcce66e5437572e9
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-09-25 16:30:42 +0500

    feat: order updated

---
+++ apps/customer_transaction/models.py
@@ -0,0 +1,30 @@
+from decimal import Decimal
+
+from django.db import models
+
+from apps.base.models import BaseModel
+
+
+class CustomerTransaction(BaseModel):
+    TransactionType = (
+        ('debt', 'Debt'),
+        ('payment', 'Payment')
+    )
+    customer = models.ForeignKey(
+        "users.User", verbose_name="Customer",
+        on_delete=models.CASCADE,
+        related_name="transactions_as_customer"
+    )
+    order = models.ForeignKey("orders.Order", on_delete=models.CASCADE, related_name="transactions")
+    amount = models.DecimalField(
+        max_digits=50, decimal_places=5,
+    )
+    currency_rate = models.DecimalField(
+        max_digits=50, decimal_places=5, default=Decimal('0.0')
+    )
+    transaction_type = models.CharField(
+        choices=TransactionType, blank=True, null=True, max_length=250
+    )
+
+    def __str__(self):
+        return f"{self.customer.name} - Payment: {self.amount}"

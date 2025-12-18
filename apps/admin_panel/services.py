6ef17c67b4456da2cf35b8227ca3ece6c7d59a22
Akhmadjon Akbarov <akhmadjonakbarov@gmail.com>
2025-12-18 17:17:42 +0500

    feat: calculate_salary

--- apps/admin_panel/services.py
+++ apps/admin_panel/services.py
@@ -1,13 +1,64 @@
-from apps.document.models import Document
+from apps.admin_panel.models import SalaryBalance, PercentageTracker
+from apps.document.models import Document, DocumentItem
+from apps.role_manager.models import Role
+from django.db.models import Prefetch
 
 
-class CalculateService:
-    def calculate_salary(self, request):
+class CalculateSalaryService:
+    def __init__(self, request, shop_id=None):
+        self.request = request
+        self.user = request.user
+        self.shop_id = shop_id
+
+        self.role = Role.objects.activate(user=self.user).first()
+        self.salary_balance = None
+
+    def get_or_create_balance(self):
+        self.salary_balance, _ = SalaryBalance.actives.get_or_create(
+            role=self.role
+        )
+
+    def calculate_salary_by_user(self):
+        self.get_or_create_balance()
+
         documents = (
-            Document.objects.filter(
-                deleted_at__isnull=True,
-                created_by=request.user,
-            ).prefetch_related(
-                ""
+            Document.actives
+            .filter(
+                created_by=self.user,
+                doc_type='sell',
+            )
+            .prefetch_related(
+                Prefetch(
+                    "document_items",
+                    queryset=(
+                        DocumentItem.actives
+                        .select_related(
+                            "product",  # if FK
+                            # OR "variant__product"
+                        )
+                    )
+                )
             )
         )
+
+        total_salary = 0
+
+        for document in documents:
+            tracker, created = PercentageTracker.objects.get_or_create(
+                document=document
+            )
+
+            if not created:
+                continue
+            document_items = (
+                document.document_items.all().prefetch_related(
+                    "product",
+                )
+            )
+            for item in document_items:
+                total_salary += item.outcome_price or 0
+
+        self.salary_balance.amount += total_salary
+        self.salary_balance.save(update_fields=["amount"])
+
+        return total_salary

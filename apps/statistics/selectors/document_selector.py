43b99770d404afb0566df88ff43eac1d75798db6
Akhmadjon Akbarov <akhmadjonakbarov@gmail.com>
2026-01-05 03:25:30 +0500

    feat: created document selector to select and filter documents by date and shop

---
+++ apps/statistics/selectors/document_selector.py
@@ -0,0 +1,33 @@
+from django.utils.dateparse import parse_date
+from apps.document.models import Document
+from apps.shops.models import Shop
+
+
+class DocumentSelector:
+
+    @staticmethod
+    def filter_documents(
+            *,
+            shop: Shop,
+            doc_type: str,
+            from_date: str | None = None,
+            to_date: str | None = None,
+    ):
+        qs = (
+            Document.actives
+            .filter(
+                shop=shop,
+                doc_type=doc_type,
+            )
+            .select_related("payment_detail")
+            .prefetch_related("document_items", "document_items__product")
+            .order_by("-created_at")
+        )
+
+        if from_date:
+            qs = qs.filter(created_at__date__gte=parse_date(from_date))
+
+        if to_date:
+            qs = qs.filter(created_at__date__lte=parse_date(to_date))
+
+        return qs
